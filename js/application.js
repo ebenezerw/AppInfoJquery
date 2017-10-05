084370556afd6a7d037f349138f384ce926c5534
ebenezerw <ebenezerw@me.com>
2017-10-05 17:23:09 -0400

    adds comments to code

--- js/application.js
+++ js/application.js
@@ -1,9 +1,14 @@
 $(document).ready(() => {
-// listen for keypress
+
+// listen for keypress - need to fix this to only listen for ENTER key (key 13)
   $("#appid").on("keypress", () => {
+
+// pass input value to variable, format, and pass to another variable. Need to update input field to have drop downs for Android or iOS apps
     var input = $("#appid").val()
     var newData = input.split(" ").join(',');
     // console.log(newData)
+
+// api call - will update to pull Android and iOS apps from single API.
     // var url = "https://itunes.apple.com/lookup?id="+newData
     var url = "https://itunes.apple.com/lookup?bundleId="+newData
     $.ajax({
@@ -11,6 +16,8 @@ $(document).ready(() => {
       dataType: 'json',
       url: url
     }).done((response) => {
+
+// pass response to variable and loop through each object and display in UI
       var apps = response.results
       $(apps).each(function (index, value) {
         $("a").attr('target','_blank');
