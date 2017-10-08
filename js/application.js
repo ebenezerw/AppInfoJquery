f675d7300f35c50efe28a2e00aca92684822f5de
ebenezerw <ebenezerw@me.com>
2017-10-08 19:28:27 -0400

    completes itunes search api

--- js/application.js
+++ js/application.js
@@ -1,16 +1,17 @@
 $(document).ready(() => {
 
 // listen for keypress - need to fix this to only listen for ENTER key (key 13)
-  $("#appid").on("keypress", () => {
+  $("#search").on("click", () => {
 
 // pass input value to variable, format, and pass to another variable. Need to update input field to have drop downs for Android or iOS apps
+    var searchType = $('input[name=searchType]:checked').val()+"=";
     var input = $("#appid").val()
     var newData = input.split(" ").join(',');
+
     // console.log(newData)
 
 // api call - will update to pull Android and iOS apps from single API. 448999087 com.thechive.Chive
-    var url = "https://itunes.apple.com/lookup?id="+newData
-    // var url = "https://itunes.apple.com/lookup?bundleId="+newData
+    var url = "https://itunes.apple.com/lookup?"+searchType+newData
     $.ajax({
       type: 'GET',
       dataType: 'json',
@@ -23,7 +24,7 @@ $(document).ready(() => {
         $("table").append(
           "<tr>" +
               "<td>" + "<img src="+value.artworkUrl60+">" + "</td>" +
-              "<td>" + value.trackName + "</td>" +
+              "<td>" + "<a href="+value.trackViewUrl+">" + value.trackName  + "</a>" + "</td>" +
               "<td>" + value.sellerName + "</td>" +
               "<td>" + value.bundleId + "</td>" +
               "<td>" + value.trackId + "</td>" +
@@ -31,13 +32,14 @@ $(document).ready(() => {
               "<td>" + value.currentVersionReleaseDate.slice(0,-10) + "</td>" +
               "<td>" + value.averageUserRating + "</td>" +
               "<td>" + value.trackContentRating + "</td>" +
-              "<td>" + "<a href="+value.trackViewUrl+">" + "Click Here" + "</a>" + "</td>" +
+              // "<td>" + "<a href="+value.trackViewUrl+">" + "Click Here" + "</a>" + "</td>" +
           "</tr>"
         )
       })
       $("a").attr('target','_blank');
       $('#appid').val(" ");
     }).fail((response) => {
+      swal("Error", "Please verify that the data entered corresponds with your 'Search by' selection", "error");
       console.log("Ajax get request failed.");
     })
   })
